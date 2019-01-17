8560a6c91a1278afd522df42c4fcbb27ebf7e144
yoanndelattre <yoanndelattre21@gmail.com>
2019-01-17 10:44:17 +0100

    add structure app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import './App.css';
+import { sampleText } from './sampleText';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Hello World !</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea 
+              value={sampleText}
+              className='form-control'
+              rows="35">
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+            <div>
+              {sampleText}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

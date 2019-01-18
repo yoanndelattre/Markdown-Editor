0bc2ec57a5c2a30a0acc6e9f919b77ee89acdb92
Yoann Delattre <yoanndelattre21@hmail.com>
2019-01-18 21:59:12 +0100

    add marked instante

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import { sampleText } from './sampleText';
+import marked from 'marked';
 
 class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  rendertext = text => {
+    const __html = marked(text, { sanitize: true })
+    return{ __html }
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
-            <textarea 
-              value={sampleText}
+            <textarea
+              onChange={this.handleChange} 
+              value={this.state.text}
               className='form-control'
               rows="35">
             </textarea>
           </div>
           <div className="col-sm-6">
-            <div>
-              {sampleText}
+            <div dangerouslySetInnerHTML={this.rendertext(this.state.text) }>
             </div>
           </div>
         </div>

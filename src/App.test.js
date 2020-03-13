acb3f6b283d8d079ac00a116eb06a95836dee3b4
yoanndelattre <yoanndelattre21@gmail.com>
2020-03-13 11:51:10 +0100

    change name require for unit test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders Markdown Translator title', () => {
+test('renders Markdown Editor title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Markdown Translator/i);
+  const linkElement = getByText(/Markdown Editor/i);
   expect(linkElement).toBeInTheDocument();
 });

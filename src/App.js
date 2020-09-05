2c842ee3c0f0839f0744223a7504a69d858c62ea
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-05 20:34:36 +0200

    change syntax clear button

--- src/App.js
+++ src/App.js
@@ -60,7 +60,9 @@ class App extends Component {
             </div>
           </div>
           <a className='report-problem' target="blank" href="https://github.com/yoanndelattre/Markdown-Editor/issues">Report a Problem</a>
-          <button className="clear-text-cache" onClick={this.handleClear}>Clear Text Cache</button>
+          <button className="clear-text-cache" onClick={this.handleClear}>
+            Clear Text Cache
+          </button>
         </div>
       </Fragment>
     );
